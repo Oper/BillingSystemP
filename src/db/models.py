f17efc675807040550f5b22800c50b0c2fcff8b9
vbaykov <my@vbaykov.ru>
2025-11-03 09:25:44 +0300

    Add new fields: model Client -> passport model Payment -> payment_date

--- src/db/models.py
+++ src/db/models.py
@@ -2,7 +2,7 @@ import enum
 from datetime import datetime
 from typing import List
 
-from sqlalchemy import func, ForeignKey
+from sqlalchemy import func, ForeignKey, JSON
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.db.database import BaseModel
@@ -32,6 +32,7 @@ class Client(BaseModel):
     connection_date: Mapped[datetime] = mapped_column(server_default=func.now())
     balance: Mapped[float] = mapped_column(default=0.0)
     is_active: Mapped[bool] = mapped_column(default=True)
+    passport: Mapped[str] = mapped_column(JSON, default=lambda: {})
     payments: Mapped[List["Payment"]] = relationship("Payment", back_populates="client", cascade="all, delete-orphan")
 
     def __repr__(self):
@@ -66,6 +67,7 @@ class Payment(BaseModel):
     """Модель платежей"""
     __tablename__ = 'payments'
     amount: Mapped[float] = mapped_column(default=0.0)
+    payment_date: Mapped[datetime] = mapped_column(server_default=func.now())
     currency: Mapped[CurrencyEnum] = mapped_column(nullable=True, default=CurrencyEnum.RUB)
     status: Mapped[StatusEnum] = mapped_column(nullable=True, default=StatusEnum.PAID)
     external_id: Mapped[str | None] = mapped_column(nullable=True)
